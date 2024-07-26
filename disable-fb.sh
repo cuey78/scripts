fe1cb70d31685859eea101f98dbec858b041cf95
cuey78 <101441768+cuey78@users.noreply.github.com>
2024-07-26 20:52:53 +1000

    Add files via upload

---
+++ disable-fb.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# 1. Download and install the QEMU hook helper from here: https://github.com/PassthroughPOST/VFIO-Tools
+# 2. Place this file in /etc/libvirt/hooks/qemu.d/NAME OF YOUR VM/prepare/begin/disable-fb.sh
+# 3. Make disable-fb.sh executable.
+
+VM_NAME="$1"
+echo "libvirt-qemu disable-fb: Disabling efi-framebuffer to prepare to pass GPU to VM $VM_NAME" > /dev/kmsg 2>&1
+echo "efi-framebuffer.0" > /sys/bus/platform/devices/efi-framebuffer.0/driver/unbind
